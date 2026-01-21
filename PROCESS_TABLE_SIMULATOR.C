44a5df2909c3f1add20ce712c9afe116ff3f7ee2
SurekhaGaddala <suga29092007@gmail.com>
2026-01-21 13:21:42 +0530

    PROCESS_TABLE_SIMULATOR.C

--- PROCESS_TABLE_SIMULATOR.C
+++ PROCESS_TABLE_SIMULATOR.C
@@ -45,19 +45,77 @@ void Bubble_sortByPriority(){
 }
 
 void Insertion_sortByPID(){
+	int i, j, comp = 0, shifts = 0;
+	Process key;
+	clock_t start = clock();
 	
+	for (i = 1; i < n; i++) {
+		key = p[i];
+		j = i - 1;
+		while (j >= 0 && p[j].pid > key.pid) {
+			comp++;
+			p[j+1] = p[j];
+			shifts++;
+			j--;
 }
 
 void Shell_sortByBST(){
+	int gap, i, j, comp = 0, shifts = 0;
+	Process temp;
+	clock_t start = clock();
 	
+	for (gap = n/2; gap > 0; gap /= 2) {
+		for (i = gap; i < n; i++) {
+			temp = p[i];
+			for(j = i; j >= gap && p[j-gap].burstTime > temp.burstTime;j-=gap)
+			{
+				comp++;
+				p[j] = p[j-gap];
+				shifts++;
+			}
+			p[j] = temp;
+		}
 }
 
 void Linear_searchByPID(){
-	
+		int key, i, comp = 0;
+	clock_t start = clock();
+	printf("\n Enter PID to be search: ");
+	scanf("%d", &key);
+	for (i=0; i<n; i++) {
+		comp++;
+		if (p[i].pid == key) {
+			clock_t end = clock();
+			printf("Found at position %d", i+1);
+			printf("\nComparisons: %d", comp);
+			printf("\nTime: %lf Clock seconds\n", (double)(end-start)/CLOCKS_PER_SEC);
+			return;
+		}
+	}
+	printf("\n Process not found");
 }
 
 void Binary_searchBYPID(){
-	
+int low = 0, high = n-1, mid, key, comp = 0;
+	clock_t start = clock();
+	printf("\nEnter PID to search: ");
+	scanf("%d", &key);
+	while (low <= high) {
+		mid = (low + high) / 2;
+		comp++;
+		if (p[mid].pid == key) {
+			clock_t end = clock();
+			printf("Found at position %d", mid+1);
+			printf("\nComparisons: %d", comp);
+			printf("\nTime: %lf seconds\n", (double)(end-start)/CLOCKS_PER_SEC);
+			return;
+		}
+		else if (key < p[mid].pid)
+		high = mid-1;
+		else
+		low = mid + 1;
+	}
+	printf("\nProcess not found");	
 }
 
 int main(){
@@ -80,3 +138,4 @@ int main(){
   } 
  }
 }
+
